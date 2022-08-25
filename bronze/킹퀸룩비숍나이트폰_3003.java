6aee339aeb0888810b135851e3b9c444fa7204bd
kimhyerim108 <92422637+kimhyerim108@users.noreply.github.com>
2022-08-25 14:51:09 +0900

    0825

---
+++ bronze/킹퀸룩비숍나이트폰_3003.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class 킹퀸룩비숍나이트폰_3003 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = {1,1,2,2,2,8};
+		for(int i =0;i<arr.length;i++)
+		{
+			int x = sc.nextInt();
+			System.out.print(arr[i] - x + " ");
+		}
+	}
+}

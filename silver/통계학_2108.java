78cd4b29a559c1ca7addf94501c91ced100e8a5c
kimhyerim108 <92422637+kimhyerim108@users.noreply.github.com>
2022-08-25 19:04:22 +0900

    0825

--- silver/통계학_2108.java
+++ silver/통계학_2108.java
@@ -1,45 +1,41 @@
+package baekjoon;
+
 import java.util.*;
 import java.lang.Math;
-public class Main
+public class 통계학_2108
 {
     static int[] arr;
     static int N;
+    static int max =1,result = 0;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
-		arr = new int[N + 1];
+		arr = new int[N+1];
 		int aver = 0;
 		for(int i=0;i<N;i++)
 		    arr[i] = sc.nextInt();
-		sort();
+		Arrays.sort(arr,0,N);
 		for(int i=0;i<N;i++)
 		    aver += arr[i];
-		mode();
+		for(int i=0;i<N;i++)
+		    System.out.println(arr[i]);
+		System.out.println("-------");
+		bin();
 		System.out.println((int)Math.round(aver / (double)N));
 		System.out.println(arr[N/2]);
-		System.out.println();
+		System.out.println(max);
 		System.out.println(arr[N - 1]-arr[0]);
 	}
-	private static void mode()
+	private static void bin()
 	{
-	    for(int i=0;i<N;i++)
-	    {
-	        arr[i]++;
-	    }
-	}
-	private static void sort()
-	{
-	    for(int i=0;i<N;i++)
-	    {
-	        for(int j=i + 1;j<N;j++)
-	        {
-	            if (arr[i] > arr[j])
-	            {
-	                int tmp = arr[j];
-	                arr[j] = arr[i];
-	                arr[i] = tmp;
-	            }
-	        }
-	    }
+		for(int i=0;i<N;i++)
+		{
+			if (arr[i] != arr[i+1])
+			{
+				if(max < result)
+					max = arr[i];
+			}
+			else result++;
+		}
 	}
 }

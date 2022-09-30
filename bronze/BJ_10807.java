98b770a49eb46ec91e2726e3962a2b3e6dde836c
hyerim108 <92422637+hyerim108@users.noreply.github.com>
2022-09-30 22:33:03 +0900

    개수세기

---
+++ bronze/BJ_10807.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) throws IOException{
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	    int N = Integer.parseInt(br.readLine());
+	    int[] arr = new int[N];
+	    int count = 0;
+	    StringTokenizer st = new StringTokenizer(br.readLine());
+	    for(int i=0;i<N;i++)
+	        arr[i] = Integer.parseInt(st.nextToken());
+	    int v = Integer.parseInt(br.readLine());
+	    for(int i=0;i<N;i++)
+	    {
+	        if (arr[i] == v)
+	            count++;
+	    }
+	    bw.write(count + "");
+	    bw.flush();
+	}
+}

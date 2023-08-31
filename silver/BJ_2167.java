7931df77be977aa8d4952223176fb987cb181455
hyerim108 <92422637+hyerim108@users.noreply.github.com>
2023-08-31 13:36:23 +0900

    BJ_2167.java

---
+++ silver/BJ_2167.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+public class Main
+{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[][] arr = new int[N+1][M+1];
+		for(int i=1;i<=N;i++)
+		{
+		    st = new StringTokenizer(br.readLine());
+		    for(int j=1;j<=M;j++)
+		        arr[i][j] = Integer.parseInt(st.nextToken());
+		}
+		int K = Integer.parseInt(br.readLine());
+		for(int a =0;a< K;a++)
+		{
+	        st = new StringTokenizer(br.readLine());
+		    int i = Integer.parseInt(st.nextToken());
+		    int j = Integer.parseInt(st.nextToken());
+		    int x = Integer.parseInt(st.nextToken());
+		    int y = Integer.parseInt(st.nextToken());
+		    int sum = 0;
+		    for(int k = i; k<=x;k++)
+		    {
+		        for(int z = j;z<=y;z++)
+		            sum += arr[k][z];
+		    }
+		    System.out.println(sum);
+		}
+	}
+}
